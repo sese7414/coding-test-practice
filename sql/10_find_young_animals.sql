336238f393817dca083c99c1a41ae5880286b740
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-05-20 09:40:39 +0900

    Create 10_find_young_animals.sql

---
+++ sql/10_find_young_animals.sql
@@ -0,0 +1,11 @@
+/*
+문제: 어린 동물 찾기
+분류: SELECT
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/59037
+*/
+
+SELECT ANIMAL_ID,
+       NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION <> 'Aged'
+ORDER BY ANIMAL_ID
