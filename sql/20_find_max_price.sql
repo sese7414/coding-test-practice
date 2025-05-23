b6e965f0b384da5ac391c6f0842b7d341e4434cc
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-05-23 10:08:09 +0900

    Create 20_find_max_price.sql

---
+++ sql/20_find_max_price.sql
@@ -0,0 +1,19 @@
+/*
+문제: 가격이 제일 비싼 식품의 정보 출력하기 - 틀림
+분류: SUM, MAX, MIN
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/131115
+*/
+
+SELECT  PRODUCT_ID,
+        PRODUCT_NAME,
+        PRODUCT_CD,
+        CATEGORY,
+        PRICE
+FROM FOOD_PRODUCT
+WHERE PRICE = (SELECT MAX(PRICE) FROM FOOD_PRODUCT) 
+/*
+select에서 바로 max()를 쓰는 것은 max()가 집계함수이기 때문에 group by를 해줘야 한다.
+그래서 where문으로 서브쿼리를 이용해서 조건을 걸어줘야 함!
+*/
+
+## 다른 방법
