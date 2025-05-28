5072d74e5a8a98360fd884bc543eaf5449798d2f
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-05-28 09:23:44 +0900

    Create 01_num_diff.py

---
+++ python/01_num_diff.py
@@ -0,0 +1,10 @@
+'''
+문제: 두 수의 차 구하기
+분류: 코딩테스트 입문
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
+'''
+def solution(num1, num2):
+    return num1 - num2
+
+# 다른 사람 풀이 중 맘에 들었던 코드
+solution = lambda num1, num2 : num1 - num2
