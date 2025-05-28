34add1119bf3eb5475fa32e6aa42073d41850784
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-05-28 09:30:04 +0900

    Update and rename 02_.py to 02_num_product.py

---
+++ python/02_num_product.py
@@ -0,0 +1,10 @@
+'''
+문제: 두 수의 곱 구하기
+분류: 코딩테스트 입문
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/120804
+'''
+def solution(num1, num2):
+    return num1*num2
+
+# 다른 방법
+solution = lambda num1, num2: num1*num2
