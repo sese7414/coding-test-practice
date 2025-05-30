1a1291f40173a846267dbb3519dd69e70f851d97
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-05-30 10:01:19 +0900

    Create 04_cal_age.py

---
+++ python/04_cal_age.py
@@ -0,0 +1,11 @@
+'''
+문제: 나이 출력
+분류: 코딩테스트 입문
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+'''
+
+def solution(age):
+    return 2022-age+1
+
+# lambda 사용
+solution = lambda age: 2022-age+1
