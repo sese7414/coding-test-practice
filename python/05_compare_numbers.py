ff8057cedf9edeb73bc3b48e2e9af53a55512549
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-06-02 10:04:42 +0900

    Create 05_compare_numbers.py

---
+++ python/05_compare_numbers.py
@@ -0,0 +1,15 @@
+'''
+문제: 숫자 비교하기
+분류: 코딩테스트 입문
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/120807
+'''
+
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1 
+
+# 내가 원했던 코드
+def solution(num1, num2):
+    return 1 if num1==num2 else -1
