61b7a3dac34f2bd8afb8070a0c442d859fa24ced
sese7414 <92248249+sese7414@users.noreply.github.com>
2025-06-04 09:48:32 +0900

    Create 34_incorrect_adoption_date.sql

---
+++ sql/34_incorrect_adoption_date.sql
@@ -0,0 +1,12 @@
+/*
+문제: 있었는데요 없었습니다
+분류: JOIN
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/59043
+*/
+
+SELECT ao.ANIMAL_ID, ao.NAME
+FROM ANIMAL_INS ai
+INNER JOIN ANIMAL_OUTS ao 
+ON ai.ANIMAL_ID = ao.ANIMAL_ID
+WHERE ai.DATETIME > ao.DATETIME
+ORDER BY ai.DATETIME -- 보호 시작일이 빠른 순
